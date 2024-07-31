b229a0311f6f9f48e49bc7808bc0145151b7849b
anishkha02 <anishkha0208@gmail.com>
2024-07-31 22:23:52 +0530

    Source code

---
+++ lib/data/database.dart
@@ -0,0 +1,20 @@
+import 'package:hive_flutter/hive_flutter.dart';
+
+class ToDoDataBase {
+  List toDoList = [];
+  final _myBox = Hive.box('mybox');
+  void createInitialData() {
+    toDoList = [
+      ["Practise questions", false],
+      ["Daily challenge", false],
+    ];
+  }
+
+  void loadData() {
+    toDoList = _myBox.get("TODOLIST");
+  }
+
+  void updateDataBase() {
+    _myBox.put("TODOLIST", toDoList);
+  }
+}
